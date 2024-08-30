35ae23a05269f23f854ec654ffcd59aeb97a6ce8
Benjamin Johnson <bjohnson@fcusd.org>
2024-08-30 09:53:46 -0700

    Update Program3.java

--- Program3.java
+++ Program3.java
@@ -1,14 +1,40 @@
-//Program 3
-//Extension of program 2 that now asks for user input when assigning the length and width of the sides
 import java.util.Scanner; //This imports the console scanner that reads user input
 
 //Your code here:
-
+public class Program3 {
+    public static void main(String[] args){
+        //field
+        int length;
+        int width;
+        int area;
+        int perimeter;
+        // end field
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the length: ");
+        length = input.nextInt();
+        System.out.println();
+        System.out.print("Enter the width: ");
+        width = input.nextInt();
+        System.out.println();
+        area = length * width;
+        perimeter = 2 * (length + width);
+        System.out.println("The Length is: " + length);
+        System.out.println("The Width is: " + width);
+        System.out.println("The Area is: " + area);
+        System.out.println("The Perimeter is: " + perimeter);
+    }
+}
 
 
 
 //Paste console output below:
 /*
+Enter the length: 37
 
+Enter the width: 26
 
+The Length is: 37
+The Width is: 26
+The Area is: 962
+The Perimeter is: 126
 */
